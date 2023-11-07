aa3418d155a52417a0d544d5c66a051b4ac47c21
hello-qiao <37826640+hello-qiao@users.noreply.github.com>
2023-11-07 18:39:02 +0800

    Update message_send.py
    update push config

--- message_send.py
+++ message_send.py
@@ -1,5 +1,80 @@
 import requests
 import json
+import json
+from typing import Optional, Union
+
+class PushDeer:
+    server = "https://api2.pushdeer.com"
+    endpoint = "/message/push"
+    pushkey = None
+
+    def __init__(self, server: Optional[str] = None, pushkey: Optional[str] = None):
+        if server:
+            self.server = server
+        if pushkey:
+            self.pushkey = pushkey
+
+    def _push(self, text: str, desp: Optional[str] = None, server: Optional[str] = None,
+              pushkey: Optional[str] = None, text_type: Optional[str] = None, **kwargs):
+
+        if not pushkey and not self.pushkey:
+            raise ValueError("pushkey must be specified")
+
+        res = self._send_push_request(desp, pushkey or self.pushkey, server or self.server, text, text_type, **kwargs)
+        if res["content"]["result"]:
+            result = json.loads(res["content"]["result"][0])
+            if result["success"] == "ok":
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def _send_push_request(self, desp, key, server, text, type, **kwargs):
+        return requests.get(server + self.endpoint, params={
+            "pushkey": key,
+            "text": text,
+            "type": type,
+            "desp": desp,
+        }, **kwargs).json()
+
+    def send_text(self, text: str, desp: Optional[str] = None, server: Optional[str] = None,
+                  pushkey: Union[str, list, None] = None, **kwargs):
+        """
+        Any text are accepted when type is text.
+        @param text: message : text
+        @param desp: the second part of the message (optional)
+        @param server: server base
+        @param pushkey: pushDeer pushkey
+        @return: success or not
+        """
+        return self._push(text=text, desp=desp, server=server, pushkey=pushkey, text_type='text', **kwargs)
+
+    def send_markdown(self, text: str, desp: Optional[str] = None, server: Optional[str] = None,
+                      pushkey: Union[str, list, None] = None, **kwargs):
+        """
+        Text in Markdown format are accepted when type is markdown.
+        @param text: message : text in markdown
+        @param desp: the second part of the message in markdown (optional)
+        @param server: server base
+        @param pushkey: pushDeer pushkey
+        @return: success or not
+        """
+        return self._push(text=text, desp=desp, server=server, pushkey=pushkey, text_type='markdown', **kwargs)
+
+    def send_image(self, image_src: str, desp: Optional[str] = None, server: Optional[str] = None,
+                   pushkey: Union[str, list, None] = None, **kwargs):
+        """
+        Only image src are accepted by API now, when type is image.
+        @param image_src: message : image URL
+        @param desp: the second part of the message (optional)
+        @param server: server base
+        @param pushkey: pushDeer pushkey
+        @return: success or not
+        """
+        return self._push(text=image_src, desp=desp, server=server, pushkey=pushkey, text_type='image', **kwargs)
+
+
 
 
 class MessageSend:
@@ -61,13 +136,16 @@ class MessageSend:
             "title": title,
             "desp": content,
         }
-        resp = requests.post(f"https://sctapi.ftqq.com/{sendkey}.send", data=payload)
-        resp_json = resp.json()
-        if resp_json["code"] == 0:
-            print(f"[ServerChan]Send message to ServerChan successfully.")
-        if resp_json["code"] != 0:
-            print(f"[ServerChan][Send Message Response]{resp.text}")
-            return -1
+        # https://push.wudixiaolong.xyz/message/push?pushkey=PDU1TtzDLwF2Y7sCPSWGOYT3rp4fwbNKl6lu6&text=%E8%A6%81%E5%8F%91%E9%80%81%E7%9A%84%E5%86%85%E5%AE%B9
+        pushdeer = PushDeer(server="https://push.wudixiaolong.xyz", pushkey="PDU1TtzDLwF2Y7sCPSWGOYT3rp4fwbNKl6lu6")
+        pushdeer.send_text(title, desp=content)
+        # resp = requests.post(f"https://sctapi.ftqq.com/{sendkey}.send", data=payload)
+        # resp_json = resp.json()
+        # if resp_json["code"] == 0:
+        #     print(f"[ServerChan]Send message to ServerChan successfully.")
+        # if resp_json["code"] != 0:
+        #     print(f"[ServerChan][Send Message Response]{resp.text}")
+        #     return -1
         return 0
 
     def weCom(self, tokens, title, content):
